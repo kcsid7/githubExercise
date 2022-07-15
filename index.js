41a39195c229b74b6e532a4c2afbef517208655e
sKc <siddharthakc10@gmail.com>
2022-07-15 15:56:18 -0400

    Added 2nd button

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 console.log("hello world!")
 
 const btn = document.querySelector("#clickMe");
+const btn2 = document.querySelector("#clickMe2");
+
+const body = document.querySelector("body");
 
 
 btn.addEventListener("click", function() {
     alert("You clicked me");
+})
+
+btn2.addEventListener("click", function() {
+    body.style.backgroundColor = "red"
+    body.style.color = "white"
+    setTimeout(function() {
+        body.style.backgroundColor = "teal"
+        body.style.color = "black"
+    }, 2000)
 })
